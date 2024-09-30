3f6c1cdfbd0d4fc842180c660d53840163f4e879
Giovanni Perniola <gioperniola1999@gmail.com>
2024-09-30 17:18:19 +0200

    tolte parentesi a somma

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let num4 = 43
 let num5 = 72
 
 let somma = num1 + num2 + num3 + num4 + num5
-let media = (somma) / 5
+let media = somma / 5
 
 console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
 
