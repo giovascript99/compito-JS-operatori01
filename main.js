aba4439a0ccc180da3c4d77a837d89b9be4c242d
Giovanni Perniola <gioperniola1999@gmail.com>
2024-09-30 17:13:02 +0200

    terminato selfwork operatori 1

---
+++ main.js
@@ -0,0 +1,14 @@
+let num1 = 3
+let num2 = 17
+let num3 = 25
+let num4 = 43
+let num5 = 72
+
+let somma = num1 + num2 + num3 + num4 + num5
+let media = (somma) / 5
+
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
+
+
+
+
